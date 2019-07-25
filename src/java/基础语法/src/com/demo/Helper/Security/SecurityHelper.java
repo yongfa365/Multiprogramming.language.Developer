3583dab91d8a94dda34ca3ef2a2b122af3118c18
vlyf柳永法 <yfliu@ctrip.com>
2019-07-25 19:29:18 +0800

    Security方法不报错

--- src/java/基础语法/src/com/demo/Helper/Security/SecurityHelper.java
+++ src/java/基础语法/src/com/demo/Helper/Security/SecurityHelper.java
@@ -3,6 +3,7 @@ package com.demo.Helper.Security;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 public class SecurityHelper {
@@ -20,40 +21,52 @@ public class SecurityHelper {
     }
 
 
-    public static String toSHA1(String input) throws Exception {
-        var inputBytes = input.getBytes(StandardCharsets.UTF_8);
-        var provider = MessageDigest.getInstance("SHA-1");
-        provider.update(inputBytes);
+    public static String toSHA1(String input) {
+        try {
+            var inputBytes = input.getBytes(StandardCharsets.UTF_8);
+            var provider = MessageDigest.getInstance("SHA-1");
+            provider.update(inputBytes);
 
-        var result = String.format("%01x", new BigInteger(1, provider.digest()));
-        return result;
+            var result = String.format("%01x", new BigInteger(1, provider.digest()));
+            return result.toUpperCase();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("toSHA1Exception", e);
+        }
     }
 
 
-    public static String toSHA512(String input) throws Exception {
-        var inputBytes = input.getBytes(StandardCharsets.UTF_8);
-        var provider = MessageDigest.getInstance("SHA-512");
-        provider.update(inputBytes);
+    public static String toSHA512(String input) {
+        try {
+            var inputBytes = input.getBytes(StandardCharsets.UTF_8);
+            var provider = MessageDigest.getInstance("SHA-512");
+            provider.update(inputBytes);
 
-        var result = String.format("%01x", new BigInteger(1, provider.digest()));
-        return result;
+            var result = String.format("%01x", new BigInteger(1, provider.digest()));
+            return result.toUpperCase();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("toSHA512Exception", e);
+        }
     }
 
 
-    public static String to16BitMD5(String input) throws Exception {
+    public static String to16BitMD5(String input) {
         var full = to32BitMD5(input);
         var result = full.substring(8, 24);
-        return result;
+        return result.toUpperCase();
     }
 
 
-    public static String to32BitMD5(String input) throws Exception {
-        var inputBytes = input.getBytes(StandardCharsets.UTF_8);
-        var provider = MessageDigest.getInstance("MD5");
-        provider.update(inputBytes);
-        //%032x=%0第一个参数，32是最终返回的最小长度，x是以16进制返回
-        var result = String.format("%032x", new BigInteger(1, provider.digest()));
-        return result;
+    public static String to32BitMD5(String input) {
+        try {
+            var inputBytes = input.getBytes(StandardCharsets.UTF_8);
+            var provider = MessageDigest.getInstance("MD5");
+            provider.update(inputBytes);
+            //%032x=%0第一个参数，32是最终返回的最小长度，x是以16进制返回
+            var result = String.format("%032x", new BigInteger(1, provider.digest()));
+            return result.toUpperCase();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("to32BitMD5Exception", e);
+        }
     }
 
 }
