1597f4f453b9ec8bbce89331a5119989e98bddc8
yongfa365 <yongfa365@gmail.com>
2020-07-19 23:10:35 +0800

    写了一点http的

--- src/javascript/基础语法/AboutHttp.js
+++ src/javascript/基础语法/AboutHttp.js
@@ -1,8 +1,61 @@
-﻿using System;
-using System.IO;
-using System.IO.Compression;
-using System.Net;
-using System.Text;
+﻿//https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
+
+fetch('https://jsonplaceholder.typicode.com/todos/1', { mode: 'no-cors' })
+    .then(response => response.json())
+    .then(json => console.log(json))
+
+//这些都是Promise,而不是最终的结果
+response.json()
+response.text()
+
+
+
+var url = "https://www.cnblogs.com";
+var filename = "D:\\1.html";
+var body = { a: 1, b: 2 };
+
+fetch(url, {mode:"no-cors"})
+    .then(rs => rs.text())
+    .then(body => console.log(body));
+
+//no-cors获取不到rs.headers,可以在一个网站请求自己的页面 来看此功能
+fetch("https://www.baidu.com/")
+    .then(rs => rs.headers.forEach(console.log));
+
+//貌似没关注是否gzip,直接就用了
+fetch("https://www.baidu.com/", { headers: { "Accept-Encoding": "gzip" } })
+    .then(rs => rs.text())
+    .then(body => console.log(body));
+
+
+// Example POST method implementation:
+async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+}
+
+postData('https://jsonplaceholder.typicode.com/posts', { answer: 42 })
+    .then(data => {
+        console.log(data); // JSON data parsed by `data.json()` call
+    });
+
 
 namespace ConsoleApp.BestPractices
 {
