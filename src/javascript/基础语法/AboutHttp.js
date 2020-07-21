dd4f14503613f505168c19f238665b3d0bc96e44
yfliu <yfliu@trip.com>
2020-07-21 10:54:01 +0800

    完成http

--- src/javascript/基础语法/AboutHttp.js
+++ src/javascript/基础语法/AboutHttp.js
@@ -1,5 +1,7 @@
 ﻿//https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 
+//response.json();response.text()这些都是Promise,而不是最终的结果
+
 fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then(response => response.json())
     .then(json => console.log(json))
@@ -8,29 +10,21 @@ fetch('https://jsonplaceholder.typicode.com/todos/1', { mode: 'no-cors' })
     .then(response => response.json())
     .then(json => console.log(json))
 
-//这些都是Promise,而不是最终的结果
-response.json()
-response.text()
 
-
-
-var url = "https://www.cnblogs.com";
-var filename = "D:\\1.html";
-var body = { a: 1, b: 2 };
-
-fetch(url, {mode:"no-cors"})
-    .then(rs => rs.text())
-    .then(body => console.log(body));
-
-//no-cors获取不到rs.headers,可以在一个网站请求自己的页面 来看此功能
+//no-cors获取不到rs.headers，可以在网站或其console里请求自己的页面 来看此功能
 fetch("https://www.baidu.com/")
     .then(rs => rs.headers.forEach(console.log));
 
-//貌似没关注是否gzip,直接就用了
+//据说新版fetch自动集成gzip，所以一般不用关注，下面这种写法也无效，具体用到再说 https://stackoverflow.com/a/14739453
 fetch("https://www.baidu.com/", { headers: { "Accept-Encoding": "gzip" } })
     .then(rs => rs.text())
     .then(body => console.log(body));
 
+
+
+
+
+var body = { a: 1, b: 2 };
 
 // Example POST method implementation:
 async function postData(url = '', data = {}) {
@@ -57,102 +51,24 @@ postData('https://jsonplaceholder.typicode.com/posts', { answer: 42 })
     });
 
 
-namespace ConsoleApp.BestPractices
-{
-    /// <summary>
-    /// 常用的操作基本都是一句话的事情，这要是在Java，你还是用第三方的HttpClient吧。
-    /// </summary>
-    public class AboutHttp
-    {
-        public static void RunDemo()
-        {
-            var url = "https://www.cnblogs.com";
-            var filename = @"D:\1.html";
-            var body = "{a:1,b:2}";
-
-
-            using (var client = new WebClient())
-            {
-                //下载二进制、文件、String,就是一句话的事情
-                client.DownloadString(url);
-                client.DownloadData(url);
-                client.DownloadFile(url, filename);
-
-                //上传二进制、文件、String,就是一句话的事情
-                client.UploadString(url, body);
-                client.UploadData(url, Encoding.UTF8.GetBytes(body));
-                client.UploadFile(url, filename);
-
-                //Async
-                client.DownloadStringTaskAsync(new Uri(url))
-                    .ContinueWith(p => Console.Write(p.Result));
-            }
-
-
-            //如有必要设置各种参数吧
-            using (var client = new WebClient())
-            {
-                client.Encoding = Encoding.UTF8;
-
-                //Request Headers
-
-                client.Headers.Add(HttpRequestHeader.Accept, "application/json, text/plain, */*");//有一堆枚举可用
-                //client.Headers.Add("Host", "www.cnblogs.com"); //有些不能设置
-                //client.Headers.Add("Connection", "keep-alive");
-                //client.Headers.Add("Content-Length", "113");
-                client.Headers.Add("Accept", "application/json, text/plain, */*");
-                client.Headers.Add("Origin", "https://www.baidu.com");
-                client.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36");
-                client.Headers.Add("Content-Type", "application/json");
-                client.Headers.Add("Referer", "https://www.cnblogs.com/");
-                client.Headers.Add("Accept-Encoding", "gzip, deflate, br");
-                client.Headers.Add("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7");
-                client.Headers.Add("Cookie", "a=1;b=3");
-
-                //Response Headers
-                var str = client.ResponseHeaders?["Set-Cookie"];
-            }
 
 
 
-        }
-
-
-    }
-
-
-
-
-    /// <summary>
-    /// 如有必要，可以重写一个新的
-    /// </summary>
-    public class GZipWebClient : WebClient
-    {
-        protected override WebRequest GetWebRequest(Uri address)
-        {
-            var request = (HttpWebRequest)base.GetWebRequest(address);
-            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
-            request.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip,deflate");
-            request.ContentType = "text/xml";
-            request.ServicePoint.ConnectionLimit = int.MaxValue;
-            request.Credentials = CredentialCache.DefaultNetworkCredentials;
-            request.Timeout = 100 * 1000;
-            request.ReadWriteTimeout = 300 * 1000;
-            request.AllowAutoRedirect = true;
-
-
-            return request;
-        }
-
-        public string UnZip(string input)
-        {
-            var data = Convert.FromBase64String(input);
-            using (var ms = new MemoryStream(data))
-            using (var gzs = new GZipStream(ms, CompressionMode.Decompress))
-            using (var sr = new StreamReader(gzs))
-            {
-                return sr.ReadToEnd();
-            }
-        }
-    }
+// 比较老的写法，xhr
+function ajax(url) {
+    console.log("正在请求：", url);
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        //没onload之前这个Promise已经返回了，只是是pending状态，只有调用resolve后才算完成
+        xhr.onload = () => {
+            console.log("请求完成，Response:", xhr.responseText);
+            resolve(xhr.responseText);
+        };
+        xhr.onerror = reject;
+        xhr.open('GET', url);
+        xhr.send();
+    });
 }
+
+await ajax("https://httpstat.us/200?预热下https");
+      
